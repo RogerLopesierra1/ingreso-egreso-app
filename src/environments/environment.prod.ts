24f240f5768d2e84c88fc4a825b661f99e36fa2b
Roger Lopesierra <rogerlopesierraolivera@gmail.com>
2019-10-12 04:50:55 -0500

    Seccion 7 Lista

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDRFhyCfKeD43WEPfzzl4nqD5el3wvzqIg',
+    authDomain: 'ingreso-egreso-app-82131.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-82131.firebaseio.com',
+    projectId: 'ingreso-egreso-app-82131',
+    storageBucket: 'ingreso-egreso-app-82131.appspot.com',
+    messagingSenderId: '1068529099783',
+    appId: '1:1068529099783:web:5b22fe07410868dbafb6a2',
+    measurementId: 'G-TL68DF7RND'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDRFhyCfKeD43WEPfzzl4nqD5el3wvzqIg',
+    authDomain: 'ingreso-egreso-app-82131.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-82131.firebaseio.com',
+    projectId: 'ingreso-egreso-app-82131',
+    storageBucket: 'ingreso-egreso-app-82131.appspot.com',
+    messagingSenderId: '1068529099783',
+    appId: '1:1068529099783:web:5b22fe07410868dbafb6a2',
+    measurementId: 'G-TL68DF7RND'
+  }
 };
 
 /*
